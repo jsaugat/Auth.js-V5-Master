0220cc85cc75cb31a255a426d5c81883c6958ca5
Saugat Joshi <jsaugatt02@gmail.com>
2024-10-18 00:00:07 +0545

    add: handle Oauth clicks using new handleClick function

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -3,14 +3,21 @@
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const Social = () => {
+  const handleClick = async (provider: "google" | "github") => {
+    // use the signIn function to sign in with a specific provider
+    // callbackUrl sets the post-authentication redirect destination for users signing in via social providers.
+    await signIn(provider, { callbackUrl: DEFAULT_LOGIN_REDIRECT });
+  }
   return (
     <div className="w-full flex flex-col md:flex-row items-center gap-2">
       <Button
         size="lg"
         variant="outline"
-        onClick={() => console.log("social button clicked")}
+        onClick={() => handleClick("google")}
         className="w-full text-xl"
       >
         <FcGoogle />
@@ -18,7 +25,8 @@ export const Social = () => {
       <Button
         size="lg"
         variant="outline"
-        onClick={() => console.log("social button clicked")}
+        onClick={() => handleClick("github")}
+
         className="w-full text-xl"
       >
         <FaGithub />
