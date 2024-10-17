5f5d3bd3a179b52f18cf024d6848bbed765a07e6
Saugat Joshi <jsaugatt02@gmail.com>
2024-10-18 02:45:52 +0545

    add: a custom error page in auth/error route

---
+++ components/auth/error-card.tsx
@@ -0,0 +1,20 @@
+import { Header } from "@/components/auth/header"
+import { BackButton } from "@/components/auth/back-button"
+import { Card, CardFooter, CardHeader } from "@/components/ui/card"
+import { CardWrapper } from "@/components/auth/card-wrapper"
+import { ExclamationTriangleIcon } from "@radix-ui/react-icons"
+
+export const ErrorCard = () => {
+  return (
+    <CardWrapper
+      headerLabel="Oops Something went wrong"
+      backButtonHref="/auth/login"
+      backButtonLabel="Back to login"
+      showSocial={false}
+    >
+      <div className="w-full flex justify-center items-center">
+        <ExclamationTriangleIcon className="text-orange-600 size-14" />
+      </div>
+    </CardWrapper>
+  )
+}
