7b4ea5f4be91d647f8ae32f7d20b225210366e23
Saugat Joshi <jsaugatt02@gmail.com>
2024-10-17 23:58:12 +0545

    add: configure for Google and Github providers

--- auth.config.ts
+++ auth.config.ts
@@ -13,12 +13,30 @@
 
 import { NextAuthConfig } from "next-auth"
 import Credentials from "next-auth/providers/credentials"
+import Github from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
 import { LoginSchema } from "@/schemas"
 import { getUserByEmail } from "./data/user"
 import bcrypt from "bcryptjs"
 
+console.log({ 'Google Client ID': process.env.AUTH_GOOGLE_ID });
+console.log({ 'Github Client ID': process.env.AUTH_GITHUB_ID });
+
 export default {
   providers: [
+    Github({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: "select_account",
+        },
+      },
+    }),
     Credentials({
       // credentials: {
       //   email: { label: "Email" },
