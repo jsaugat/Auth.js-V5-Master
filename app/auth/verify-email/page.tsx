f15cb4ef43aeb147823a9f8ed23dd4dabb1b41d0
Saugat Joshi <jsaugatt02@gmail.com>
2024-10-19 10:52:40 +0545

    create: email confirmation page

--- app/auth/verify-email/page.tsx
+++ app/auth/verify-email/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { Button } from '@/components/ui/button'
+import { Check } from 'lucide-react'
 import { useSearchParams } from 'next/navigation'
 import React from 'react'
 
@@ -7,6 +9,12 @@ export default function VerifyEmail() {
   const searchParams = useSearchParams()
   const token = searchParams.get('token')
   return (
-    <div>Verify email {token}</div>
+    <div className='flex flex-col items-center gap-4'>
+      <p>FYI, your account verification token is : <span className="text-neutral-400">{token}</span></p>
+      <Button className="border border-border/10 hover:border-border/50 flex gap-2">
+        <Check className='size-4' />
+        Confirm email
+      </Button>
+    </div>
   )
 }
