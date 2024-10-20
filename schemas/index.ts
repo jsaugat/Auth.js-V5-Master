d2dfa9667bb33175020d306fbdd33fb7acb36bb0
Saugat Joshi <jsaugatt02@gmail.com>
2024-10-20 23:44:45 +0545

    add: resetSchema in the schemas/index.ts

--- schemas/index.ts
+++ schemas/index.ts
@@ -9,3 +9,6 @@ export const RegisterSchema = z.object({
   password: z.string().min(6, { message: "Password must be at least 6 characters." }),
   name: z.string().min(1, { message: "Name is required." }),
 })
+export const ResetSchema = z.object({
+  email: z.string().email({ message: "Invalid email address." }),
+})
