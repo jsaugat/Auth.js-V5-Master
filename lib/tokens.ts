ac161fbca88990e65e20f28262eaa700d97977b8
Saugat Joshi <jsaugatt02@gmail.com>
2024-10-22 10:48:05 +0545

    add: new function to generate password reset token

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,3 +1,4 @@
+import { getPasswordResetTokenByEmail } from "@/data/password-reset-token";
 import { getVerificationTokenByEmail } from "@/data/verification-token";
 import { db } from "@/lib/db";
 import { v4 as uuidv4 } from 'uuid';
@@ -27,3 +28,30 @@ export const generateVerificationToken = async (email: string) => {
   })
   return newVerificationToken;
 } 
+
+// Generate a password reset token for a given email address.
+export const generatePasswordResetToken = async (email: string) => {
+  const token = uuidv4();
+  const expires_at = new Date(Date.now() + 1000 * 60 * 60 * 24); // 24 hours from now
+
+  // Check if a token already exists for the email.
+  const existingToken = await getPasswordResetTokenByEmail(email);
+  // Delete it if it exists.
+  if (existingToken) {
+    await db.passwordResetToken.delete({
+      where: {
+        id: existingToken.id // using id because it is a unique identifier, keeps data efficiency
+      }
+    })
+  }
+
+  //? Create a new password reset token if one does not exist.
+  const newPasswordResetToken = await db.passwordResetToken.create({
+    data: {
+      email,
+      token,
+      expires_at
+    }
+  })
+  return newPasswordResetToken;
+}
