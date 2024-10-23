7967deff27dec61552d36ba985f998bc673716a9
Saugat Joshi <jsaugatt02@gmail.com>
2024-10-23 16:37:22 +0545

    add: new generateTwoFActorToken lib funciton in the tokens file

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -2,6 +2,8 @@ import { getPasswordResetTokenByEmail } from "@/data/password-reset-token";
 import { getVerificationTokenByEmail } from "@/data/verification-token";
 import { db } from "@/lib/db";
 import { v4 as uuidv4 } from 'uuid';
+import crypto from "crypto";
+import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
 
 // Generarte a verification token for a given email address.
 export const generateVerificationToken = async (email: string) => {
@@ -27,7 +29,7 @@ export const generateVerificationToken = async (email: string) => {
     }
   })
   return newVerificationToken;
-} 
+}
 
 // Generate a password reset token for a given email address.
 export const generatePasswordResetToken = async (email: string) => {
@@ -54,4 +56,31 @@ export const generatePasswordResetToken = async (email: string) => {
     }
   })
   return newPasswordResetToken;
+}
+
+// Generate a two-factor token for a given email address.
+export const generateTwoFactorToken = async (email: string) => {
+  const token = crypto.randomInt(100_000, 1_000_000).toString();
+  const expires_at = new Date(Date.now() + 1000 * 60 * 5); // 5 minutes from now
+
+  //? Check if a token already exists for the email.
+  const existingToken = await getTwoFactorTokenByEmail(email);
+  if (existingToken) {
+    // Delete it if it exists.
+    await db.twoFactorToken.delete({
+      where: {
+        id: existingToken.id
+      }
+    })
+  }
+
+  //? Create a new two-factor token if one does not exist.
+  const newTwoFactorToken = await db.twoFactorToken.create({
+    data: {
+      email,
+      token,
+      expires_at
+    }
+  })
+  return newTwoFactorToken;
 }
