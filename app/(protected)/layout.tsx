a94fa70d97ba874bd4962d5a8538052c41dfae00
Saugat Joshi <jsaugatt02@gmail.com>
2024-10-24 23:32:06 +0545

    add: layout file to the 'protected' group

---
+++ app/(protected)/layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Navbar } from './_components/navbar'
+
+interface ProtectedLayoutProps {
+  children: React.ReactNode
+}
+
+export default function ProtectedLayout({ children }: ProtectedLayoutProps) {
+  return (
+    <div className='w-full h-full flex flex-col gap-y-10 items-center justify-center'>
+      <Navbar />
+      {children}
+    </div>
+  )
+}
