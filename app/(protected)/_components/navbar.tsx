207d8a091d88ef07fb60199be611fb2afef3b975
Saugat Joshi <jsaugatt02@gmail.com>
2024-10-24 23:32:22 +0545

    add: navbar file

---
+++ app/(protected)/_components/navbar.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import UserAvatarButton from '@/components/auth/user-avatar-button';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react'
+
+const navItems = [
+  { href: "/client", label: "Client" },
+  { href: "/server", label: "Server" },
+  { href: "/settings", label: "Settings" },
+];
+
+export const Navbar = () => {
+  const pathname = usePathname();
+  return (
+    <nav className='bg-secondary text-secondary-foreground p-3 rounded-md w-full md:w-[600px] shadow-sm flex justify-between items-center'>
+      <div className="flex gap-x-2">
+        {navItems.map(({ href, label }, idx) =>
+          <Button
+            key={idx}
+            variant={pathname === href ? "default" : "outline"}
+            className='shadow-md'
+            asChild
+          >
+            <Link href={href}>
+              {label}
+            </Link>
+          </Button>
+        )}
+      </div>
+      <UserAvatarButton />
+    </nav>
+  )
+}
